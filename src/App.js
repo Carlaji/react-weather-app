347c720520380d978147e186193a8c242918988c
Carla <u1971612@campus.eram.cat>
2023-08-30 12:10:20 +0200

    Added the Github repository url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/carla-jimenez-109852212/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Carla Jimenez
+        </a>{" "}
+        and is
+        <a
+          href="https://github.com/Carlaji/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
